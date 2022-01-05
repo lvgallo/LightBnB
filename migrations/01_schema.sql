a8461e5a77f0bbd7ad13532cdf2c3eb4e457900c
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-01-05 05:30:04 +0000

    insert data from seeds02

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -18,22 +18,22 @@ CREATE TABLE properties (
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
-  url_thumbnail_photo TEXT NOT NULL,
-  url_cover_photo TEXT NOT NULL,
+  thumbnail_photo_url TEXT NOT NULL,
+  cover_photo_url TEXT NOT NULL,
   country TEXT NOT NULL,
   street TEXT NOT NULL, 
   city TEXT NOT NULL,
   province TEXT NOT NULL,
   post_code TEXT NOT NULL,
-  is_active BOOLEAN NOT NULL DEFAULT TRUE,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE 
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE 
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE 
 );
 
@@ -41,7 +41,7 @@ CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
 );
